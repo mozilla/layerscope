bdf3a68e54c5ffc5abb134ac20c949baac30ff65
CJKu <cku@mozilla.com>
2015-05-21 17:35:35 +0800

    Bug 1163908 - [LayerScope] Add Heat map in viewer
    1. Correct z-index of setting dialog
    2. Add Draw grid of quads setting option.
    3. Remove layer tree and buffer caption to give more room to real data.
    4. Add a 3D button to swtich between 2D and 3D heap view.
    5. 3D heap-map view
    6. Pasrse and conver draw data into frame in dataprocess module.
    7. Improve 2D canvas drawing performance while zooming.

--- js/dataprocesser_proxy.js
+++ js/dataprocesser_proxy.js
@@ -1,7 +1,7 @@
 /* vim:set ts=2 sw=2 sts=2 et: */
 /* This Source Code Form is subject to the terms of the Mozilla Public
- *  * License, v. 2.0. If a copy of the MPL was not distributed with this
- *   * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 // Namespace for LayerScope globals
 if (typeof LayerScope == "undefined" || !LayerScope) {
@@ -11,16 +11,20 @@ if (typeof LayerScope == "undefined" || !LayerScope) {
 // Process sockect data from the profile target.
 LayerScope.ProtoDataProcesserProxy = {
   _graph: null,
+  _worker: null,
+
   begin: function PDP_begin(graph) {
     this.end();
     this._graph = graph;
-    this._worker = new Worker('js/dataprocesser_worker.js');
-    this._worker.onmessage = this.receiveMessage.bind(this);
   },
 
   end: function PDP_end() {
     this._graph = null;
-    this._receivingFrame = null;
+    if (typeof LayerWorker == "undefined") {
+      this._worker.postMessage({command: 'end'});
+    } else {
+      LayerWorker.OnMessage({ data: { command: 'end' } });
+    }
   },
 
   receiveMessage: function PDP_receiveMessage(e) {
@@ -35,8 +39,18 @@ LayerScope.ProtoDataProcesserProxy = {
   },
 
   input: function PDP_input(data) {
-    this._worker.postMessage(data, [data]);
+    if (typeof LayerWorker == "undefined") {
+      this._worker.postMessage({pbuffer: data}, [data]);
+    } else {
+      LayerWorker.OnMessage({ data: { pbuffer: data } });
+    }
   },
 };
+
+if (typeof LayerWorker == "undefined") {
+  LayerScope.ProtoDataProcesserProxy._worker = new Worker('js/dataprocesser_worker.js');
+  LayerScope.ProtoDataProcesserProxy._worker.onmessage =
+    LayerScope.ProtoDataProcesserProxy.receiveMessage.bind(LayerScope.ProtoDataProcesserProxy);
+}
 
 LayerScope.DataProcesserNode.register(LayerScope.ProtoDataProcesserProxy)
